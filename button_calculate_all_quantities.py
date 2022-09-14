60c021d1b73dba3d0dc586e5952849f720d7d014
Massimo Fabbro <maxfb87@yahoo.it>
2022-09-14 23:08:11 +0200

    Button to calculate all quantities now works with selected objects

--- button_calculate_all_quantities.py
+++ button_calculate_all_quantities.py
@@ -1,33 +1,62 @@
 from ifcopenshell import util
+from blenderbim.bim.module.pset.qto_calculator import QtoCalculator
 import ifcopenshell
 from blenderbim.bim.ifc import IfcStore
 import bpy
 
-objects = bpy.context.selected_objects
+selected_objects = bpy.context.selected_objects
 
 ifc = ifcopenshell.open(IfcStore.path)
+
+calculator = QtoCalculator()
 
 pset_qto = util.pset.PsetQto('IFC4')
 print('\n')
 
-for object in objects:
+for object in selected_objects:
     bpy.context.view_layer.objects.active = object
 
-    object_id = object.BIMObjectProperties.ifc_definition_id
-    ifc_object = ifc.by_id(object_id)
+    object_ifc_id = object.BIMObjectProperties.ifc_definition_id
+    ifc_object_instance = ifc.by_id(object_ifc_id)
 
-    object_type = ifc_object.get_info()['type']
+    pset_qto_ifc_instances = ifcopenshell.util.element.get_psets(ifc_object_instance, qtos_only = True)
 
-    applicable_names = pset_qto.get_applicable_names(object_type)
+    if not pset_qto_ifc_instances:
+        continue
 
-    for applicable_name in applicable_names:
-        if 'Qto_' in applicable_name:
-            qto_property_name = applicable_name
+    ifc_object_type = ifc_object_instance.get_info()['type']
 
-    qto_properties = pset_qto.get_by_name(qto_property_name).get_info()['HasPropertyTemplates']
-    #name = pset_qto.get_by_name('Qto_WallBaseQuantities').get_info()['HasPropertyTemplates'][1].get_info()['Name']
+    applicable_pset_names = pset_qto.get_applicable_names(ifc_object_type)
 
-    for qto_property in qto_properties:
-        print(qto_property.get_info()['Name'])
+    print(applicable_pset_names)
+
+    for applicable_pset_name in applicable_pset_names:
+        if 'Qto_' in applicable_pset_name:
+            pset_qto_name = applicable_pset_name
+
+    pset_qto_properties = pset_qto.get_by_name(pset_qto_name).get_info()['HasPropertyTemplates']
+
+    for pset_qto_property in pset_qto_properties:
+        print(pset_qto_property.get_info()['Name'])
+        quantity_name = pset_qto_property.get_info()['Name']
+        #TODO add alternative prop name
+
+        new_quantity = calculator.guess_quantity(quantity_name, quantity_name, object)
+
+        if not new_quantity:
+            new_quantity = 0
+        else:
+            new_quantity = round(new_quantity, 3)
+
+        file = IfcStore.get_file() #TODO check if can be used ifc variable defined before
+
+
+        pset_qto_id = file.by_id(pset_qto_ifc_instances[pset_qto_name]['id']) #TODO check if i can use line above
+
+        ifcopenshell.api.run("pset.edit_qto",
+                file,
+                **{"qto" : pset_qto_id, "name" : pset_qto_name, "properties": {quantity_name : new_quantity}}
+            )
+
 
 
