237a18de9a1a21e29d4f74811b6d3014245dcda2
Massimo Fabbro <maxfb87@yahoo.it>
2022-09-17 17:08:30 +0200

    Better functions structure and variable name

--- button_calculate_all_quantities.py
+++ button_calculate_all_quantities.py
@@ -18,8 +18,9 @@ class qtoAllQuantitiesCalculator():
 
             self.set_active_object(object)
 
-            self.pset_qto_ifc_instance = self.get_pset_qto_ifc_instance(object)
-            if not self.pset_qto_ifc_instance:
+            self.pset_qto_object_ifc_instance = self.get_pset_qto_object_ifc_instance(object)
+
+            if not self.pset_qto_object_ifc_instance:
                 print("There is no pset qto instance associated to object " + object.name)
                 continue
 
@@ -27,7 +28,7 @@ class qtoAllQuantitiesCalculator():
 
             for pset_qto_property in self.pset_qto_properties:
                 quantity_name = pset_qto_property.get_info()['Name']
-                #TODO add alternative prop name
+                #TODO add alternative prop name option
 
                 new_quantity = self.calculator.guess_quantity(quantity_name, quantity_name, object)
 
@@ -36,19 +37,21 @@ class qtoAllQuantitiesCalculator():
                 else:
                     new_quantity = round(new_quantity, 3)
 
-                self.pset_qto_id = self.get_pset_qto_id(object)
-
                 #TODO Check if pset_qto_name can be global
 
                 self.edit_qto(object, quantity_name, new_quantity)
 
+
     def set_active_object(self, object):
         bpy.context.view_layer.objects.active = object
 
-    def get_pset_qto_ifc_instance(self, object):
+    def get_ifc_object_instance(self, object):
         object_ifc_id = object.BIMObjectProperties.ifc_definition_id
         ifc_object_instance = self.file.by_id(object_ifc_id)
+        return ifc_object_instance
 
+    def get_pset_qto_object_ifc_instance(self, object):
+        ifc_object_instance = self.get_ifc_object_instance(object)
         pset_qto_ifc_instance = ifcopenshell.util.element.get_psets(ifc_object_instance, qtos_only = True)
         return pset_qto_ifc_instance
 
@@ -57,6 +60,12 @@ class qtoAllQuantitiesCalculator():
         pset_qto_properties = self.pset_qto.get_by_name(pset_qto_name).get_info()['HasPropertyTemplates']
         return pset_qto_properties
 
+    def get_applicable_pset_names(self, object):
+        ifc_object_instance = self.get_ifc_object_instance(object)
+        ifc_object_type = ifc_object_instance.get_info()['type']
+        applicable_pset_names = self.pset_qto.get_applicable_names(ifc_object_type)
+        return applicable_pset_names
+
     def get_pset_qto_name(self, object):
         applicable_pset_names = self.get_applicable_pset_names(object)
         for applicable_pset_name in applicable_pset_names:
@@ -64,23 +73,18 @@ class qtoAllQuantitiesCalculator():
                 pset_qto_name = applicable_pset_name
                 return pset_qto_name
 
-    def get_applicable_pset_names(self, object):
-        object_ifc_id = object.BIMObjectProperties.ifc_definition_id
-        ifc_object_instance = self.file.by_id(object_ifc_id)
-        ifc_object_type = ifc_object_instance.get_info()['type']
-        applicable_pset_names = self.pset_qto.get_applicable_names(ifc_object_type)
-        return applicable_pset_names
-
     def get_pset_qto_id(self, object):
         pset_qto_name = self.get_pset_qto_name(object)
-        pset_qto_id = self.file.by_id(self.pset_qto_ifc_instance[pset_qto_name]['id']) #TODO check if i can use line above
+        pset_qto_id = self.file.by_id(self.pset_qto_object_ifc_instance[pset_qto_name]['id']) #TODO check if i can use line above
         return pset_qto_id
 
     def edit_qto(self, object, quantity_name, new_quantity):
         pset_qto_name = self.get_pset_qto_name(object)
+        pset_qto_id = self.get_pset_qto_id(object)
+
         ifcopenshell.api.run("pset.edit_qto",
                 self.file,
-                **{"qto" : self.pset_qto_id, "name" : pset_qto_name, "properties": {quantity_name : new_quantity}}
+                **{"qto" : pset_qto_id, "name" : pset_qto_name, "properties": {quantity_name : new_quantity}}
             )
 
 qto_all_quantities_calculator = qtoAllQuantitiesCalculator()
